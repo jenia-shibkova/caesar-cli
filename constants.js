348b664903adadc7d30146e2d69e67e19b3141c3
Eugenia Shibkova <shibkova91@gmail.com>
2020-10-06 03:36:48 +0300

    refactor: modify constants

--- constants.js
+++ constants.js
@@ -1,3 +1,10 @@
 exports.ENCODE = 'encode';
 exports.DECODE = 'decode';
 exports.OUTPUT_FILE_NAME = 'output.txt';
+exports.LETTER_CHAR_CODES = {
+  A: 'A'.charCodeAt(), // 65
+  a: 'a'.charCodeAt(), // 97
+  Z: 'Z'.charCodeAt(), // 90
+  z: 'z'.charCodeAt() // 122
+};
+exports.ERROR_MESSAGE = 'No such file or directory. Check input value.';
